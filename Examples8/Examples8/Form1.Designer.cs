57baa5ff7c95d491019bee06b2f6f252b4451dee
TheGreatGhaderi <123986000+TheGreatGhaderi@users.noreply.github.com>
2023-01-30 22:56:05 +0330

    Add files via upload

---
+++ Examples8/Examples8/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Examples8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalc.Location = new System.Drawing.Point(99, 152);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(90, 33);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "محاسبه";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.BtnCalc_Click);
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAge.Location = new System.Drawing.Point(48, 43);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(194, 29);
+            this.lblAge.TabIndex = 1;
+            this.lblAge.Text = "سن خود را وارد کنید";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(74, 95);
+            this.txtAge.Multiline = true;
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(132, 32);
+            this.txtAge.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 266);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.btnCalc);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Calc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.TextBox txtAge;
+    }
+}
+
---
+++ Examples8/Examples8/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examples8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalc_Click(object sender, EventArgs e)
+        {
+            int d = Convert.ToInt32(txtAge.Text);
+            int y, m, w;
+            y = d / 365;
+            d %= 365;
+            m = d / 30;
+            d %= 30;
+            w = d / 7;
+            d %= 7;
+            MessageBox.Show("You Have " + y.ToString() + " Years And " + m.ToString() + " Months And " + w.ToString() + " Weeks And " + d.ToString() + " Days In Your Life.\n Enjoy It =))");
+        }
+    }
+}

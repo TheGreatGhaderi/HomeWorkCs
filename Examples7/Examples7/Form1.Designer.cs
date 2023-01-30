b6a03941f3d170f61a50d4b24502ca5030d4ff9d
TheGreatGhaderi <123986000+TheGreatGhaderi@users.noreply.github.com>
2023-01-30 22:37:17 +0330

    Add files via upload

---
+++ Examples7/Examples7/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Examples7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.txtTavaan = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalc.Location = new System.Drawing.Point(114, 145);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(113, 29);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "محاسبه";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.BtnCalc_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(25, 67);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(157, 29);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "عدد را وارد کنید";
+            // 
+            // txtTavaan
+            // 
+            this.txtTavaan.Location = new System.Drawing.Point(198, 70);
+            this.txtTavaan.Multiline = true;
+            this.txtTavaan.Name = "txtTavaan";
+            this.txtTavaan.Size = new System.Drawing.Size(122, 32);
+            this.txtTavaan.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 305);
+            this.Controls.Add(this.txtTavaan);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnCalc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.TextBox txtTavaan;
+    }
+}
+
---
+++ Examples7/Examples7/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examples7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalc_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(txtTavaan.Text);
+            double x2 = Math.Pow(x, 2);
+            double x3 = Math.Pow(x, 3);
+            MessageBox.Show("Tavaan 2: " + x2.ToString() + "\nTavaan 3: " + x3.ToString());
+        }
+    }
+}
